0ea3b3875ebc9e6703c400d59a3185172aaf9a16
deforu <22j1162@andrew.ac.jp>
2024-07-19 06:55:25 +0900

    APIのベースURLを環境変数で設定

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const client = axios.create({
-  baseURL: "http://localhost:3001/api/v1"
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:3001/api/v1"
 })
 
 export default client
